b4010cc055f0a06a2077303f2a21c9e2e3806802
Perpetuo Cambio <boloboje@gmail.com>
2025-07-22 21:17:03 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://perpetuocambio.github.io',
+  site: 'https://perpetuocambio.github.io/versos-de-protesta',
   base: '/versos-de-protesta/',
   output: 'static',
   markdown: {
